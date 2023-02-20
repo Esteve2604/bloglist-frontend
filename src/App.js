b47db6546093c723c0c49fb217c1ef3f214ad87b
Esteve2604 <estebanasperuiz@gmail.com>
2023-02-20 13:46:47 +0100

    parte 5-1 sin comprobar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
+import Blogs from './components/Blogs'
+import Login from './components/Login'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null ?
+      <Login username={username} password={password} user={user}
+        setUsername={setUsername} setPassword={setPassword} setUser={setUser} />
+      :
+      <Blogs blogs={blogs} />}
     </div>
   )
 }
